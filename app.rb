f05dd187fb308acad2a16f300cd77c59c5f439b0
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-04 17:43:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,4 +22,21 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5]
   end
+
+  get '/:operation/:number1/:number2' do
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+    result = 0
+    case params[:operation]
+      when "add"
+        result = n1 + n2
+      when "subtract"
+        result = n1 - n2
+      when "multiply"
+        result = n1 * n2
+      when "divide"
+        result = n1 /n2
+    end
+    result
+  end
 end
