17de1cdbcd166b0a5849f35ab97cc4e0df9f5402
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-04 17:13:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,4 +10,11 @@ class App < Sinatra::Base
   get '/square/:number' do
     "#{params[:number].to_i ** 2}"
   end
+
+  get '/say/:number/:phrase' do
+    params[:number].to_i.times{
+      "#{params[:phrase]}"
+    }
+
+  end
 end
